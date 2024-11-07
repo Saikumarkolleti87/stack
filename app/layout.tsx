e5f894fe246dccc5f2a5af7c954609836f707fa5
Saikumarkolleti87 <saikumarkolleti87@gmail.com>
2024-11-07 12:25:42 -0500

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 import React from "react";
+
+import "./globals.css";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
   variable: "--font-inter",
-  weight: "100 200 300 400 500 600 700 800 900",
+  weight: "100 200 300 400 500 700 800 900",
 });
 
 const spaceGrotesk = localFont({
-  src: "./fonts/SpaceGrotesk.ttf",
+  src: "./fonts/SpaceGroteskVF.ttf",
   variable: "--font-space-grotesk",
-  weight: "300 400 500 600 700",
+  weight: "300 400 500 700",
 });
 
 export const metadata: Metadata = {
-  title: "Dev Overflow",
+  title: "DevFlow",
   description:
     "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
   icons: {
