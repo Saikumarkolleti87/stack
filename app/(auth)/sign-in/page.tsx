2dcc6af7565065d21b612edc46adb97fb0ba0561
Saikumarkolleti87 <saikumarkolleti87@gmail.com>
2024-11-15 09:33:32 -0500

    Auth

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,18 @@
+"use client";
 import React from "react";
 
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInScheme } from "@/lib/validations";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInScheme}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default SignIn;
